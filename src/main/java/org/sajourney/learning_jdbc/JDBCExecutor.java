99ac0c5c90fddbedc038c8921e6759f541fcce47
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-21 09:07:53 +0300

    setting up database connection manager

--- src/main/java/org/sajourney/learning_jdbc/JDBCExecutor.java
+++ src/main/java/org/sajourney/learning_jdbc/JDBCExecutor.java
@@ -3,6 +3,10 @@ package org.sajourney.learning_jdbc;
 public class JDBCExecutor {
     public static void main(String[] args){
         System.out.println("Hello Learning JDBC");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+        try{
+
+        }
 
     }
 }
