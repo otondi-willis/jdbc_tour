a949442dd7c606aadbd7bd7ac121803cad2b53ac
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-21 08:35:55 +0300

    created a constructor

--- src/main/java/org/sajourney/learning_jdbc/DatabaseConnectionManager.java
+++ src/main/java/org/sajourney/learning_jdbc/DatabaseConnectionManager.java
@@ -1,5 +1,19 @@
 package org.sajourney.learning_jdbc;
 
 public class DatabaseConnectionManager {
+    private final String url;
+    private final Properties properties;
+
+    public DatabaseConnectionManager(String host,
+                                     String databaseName,
+                                     String username,
+                                     String password){
+        this.url = "jdbc:postgressql://"+host+"/"+databaseName;
+        this.properties = new
+
+    }
+
+
+
 
 }
