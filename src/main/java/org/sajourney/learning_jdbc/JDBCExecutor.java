bf572d6775724aa58e39fcf26c76672f5e5d1a23
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-21 09:20:52 +0300

    password rectification

--- src/main/java/org/sajourney/learning_jdbc/JDBCExecutor.java
+++ src/main/java/org/sajourney/learning_jdbc/JDBCExecutor.java
@@ -1,11 +1,25 @@
 package org.sajourney.learning_jdbc;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JDBCExecutor {
     public static void main(String[] args){
         System.out.println("Hello Learning JDBC");
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "password");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport", "postgres", "12345");
         try{
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT (*) FROM CUSTOMER");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1));
+            }
 
+
+        } catch (SQLException e){
+            e.printStackTrace();
         }
 
     }
