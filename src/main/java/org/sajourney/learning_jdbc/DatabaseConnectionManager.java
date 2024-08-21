369e34af450246c197288d073b2b013b4187f38b
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-21 09:05:03 +0300

    db connection functionality

--- src/main/java/org/sajourney/learning_jdbc/DatabaseConnectionManager.java
+++ src/main/java/org/sajourney/learning_jdbc/DatabaseConnectionManager.java
@@ -1,6 +1,9 @@
 package org.sajourney.learning_jdbc;
 
-import org.postgresql.util.PSQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnectionManager {
     private final String url;
@@ -10,17 +13,13 @@ public class DatabaseConnectionManager {
                                      String databaseName,
                                      String username,
                                      String password){
-        this.url = "jdbc:postgressql://"+host+"/"+databaseName;
+        this.url = "jdbc:postgresql://" + host + "/" + databaseName;
         this.properties = new Properties();
         this.properties.setProperty("user", username);
         this.properties.setProperty("password", password);
-
     }
+
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(this.url, this.properties);
     }
-
-
-
-
 }
