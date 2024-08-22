0a9d3fd17f0a15adfbd83925cf1e008ffc3fde82
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-22 04:26:49 +0300

    implementing interface's method

--- src/main/java/org/sajourney/learning_jdbc/CustomerDAO.java
+++ src/main/java/org/sajourney/learning_jdbc/CustomerDAO.java
@@ -2,5 +2,31 @@ package org.sajourney.learning_jdbc;
 
 import org.sajourney.learning_jdbc.util.DataAccessObject;
 
+import java.util.List;
+
 public class CustomerDAO extends DataAccessObject<Customer> {
+    @Override
+    public Customer findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @Override
+    public Customer update(Customer dto) {
+        return null;
+    }
+
+    @Override
+    public Customer create(Customer dto) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
 }
